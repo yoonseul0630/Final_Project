5aa45ada7ae904c450226eacba4b755257eefa73
“yoonseul0630” <y200630@naver.com>
2026-05-07 13:16:48 +0900

    ㄴ

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
   try {
     const { username, password } = JSON.parse(event.body);
 
-    const INTERNAL_API_URL = "https://wallpapers-exhibition-cancellation-speeches.trycloudflare.com/auth";
+    const INTERNAL_API_URL = "https://traveling-walks-formation-summaries.trycloudflare.com/auth";
 
     
     const response = await axios.post(INTERNAL_API_URL, {
